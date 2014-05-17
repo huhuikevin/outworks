88035d5e20d04056370aac5d881c7a3fcb797002
kevinhuhui <huhuikevin@gmail.com>
2014-05-17 14:01:38 +0800

    add timer16
    Signed-off-by: kevinhuhui <huhuikevin@gmail.com>

---
+++ include/timer16n.h
@@ -0,0 +1,90 @@
+/**************************************************************************
+* 版权声明：Copyright@2014 上海海尔集成电路有限公司
+* 文件名称：timer.h
+* 功能描述：定时器函数声明		
+* 函数列表：init_t16g2
+            init_t16g1
+            delay_ms
+            sys_tick_init
+            sys_tick_run
+* 文件作者：sundy  	
+* 文件版本：1.0 			
+* 完成日期：2014-04-24			
+* 修订历史：
+* 修订日期：
+**************************************************************************/
+#include "type.h"
+
+#ifndef _TIMER_H_
+#define _TIMER_H_
+#define DELAY1NOP() NOP();
+#define DELAY2NOP() DELAY1NOP();NOP();
+#define DELAY3NOP() DELAY2NOP();NOP();
+#define DELAY4NOP() DELAY3NOP();NOP();
+#define DELAY5NOP() DELAY4NOP();NOP();
+#define DELAY6NOP() DELAY5NOP();NOP();
+#define DELAY7NOP() DELAY6NOP();NOP();
+#define DELAY8NOP() DELAY7NOP();NOP();
+#define DELAY9NOP() DELAY8NOP();NOP();
+
+#define DELAY10NOP() DELAY9NOP();NOP();   
+#define DELAY20NOP() DELAY10NOP();DELAY10NOP();
+#define DELAY30NOP() DELAY20NOP();DELAY10NOP();
+#define DELAY40NOP() DELAY30NOP();DELAY10NOP();
+#define DELAY50NOP() DELAY40NOP();DELAY10NOP();
+#define DELAY60NOP() DELAY50NOP();DELAY10NOP();
+#define DELAY70NOP() DELAY60NOP();DELAY10NOP();
+#define DELAY80NOP() DELAY70NOP();DELAY10NOP();
+#define DELAY90NOP() DELAY80NOP();DELAY10NOP();
+
+#define DELAY100NOP() DELAY90NOP();DELAY10NOP(); 
+
+uint8_t     _t16g1_valid;   //用于判定比较器设置时刻
+
+uint8_t     _sys_flag;      //系统时隙指示0非工作时隙1工作时隙
+uint16_t    _sys_tick;      //以过零点为基准的系统时钟
+uint16_t    _sys_last;      //上一时隙记录
+
+/**************************************************************************
+* 函数名称：init_t16g2
+* 功能描述：T16G2定时器工作模式设置为捕获模式
+* 输入参数：无
+* 返回参数：无
+**************************************************************************/
+void init_t16g2(void);
+
+/**************************************************************************
+* 函数名称：init_t16g1
+* 功能描述：T16G1定时器工作模式设置为比较器模式
+* 输入参数：type: 0 延时7ms
+                  1 延时10ms 
+* 返回参数：无
+**************************************************************************/
+void init_t16g1(uint8_t type);
+
+/**************************************************************************
+* 函数名称：delay_ms
+* 功能描述：毫秒级延时函数
+* 输入参数：ms   延时毫秒数，输入范围0-6553
+* 返回参数：无
+**************************************************************************/
+void delay_ms(uint16_t ms);
+
+/**************************************************************************
+* 函数名称：sys_tick_init
+* 功能描述：系统时钟初始化
+* 输入参数：无
+* 返回参数：无
+**************************************************************************/
+void sys_tick_init(void);
+
+/**************************************************************************
+* 函数名称：sys_tick
+* 功能描述：系统时间片识别
+* 输入参数：无
+* 返回参数：无
+**************************************************************************/
+void sys_tick_run(void);
+
+#endif
+
--- system/system_framework.c
+++ system/system_framework.c
@@ -3,7 +3,7 @@
 #include "type.h"
 #include "system.h"
 #include "tool.h"
-#include "timer8n.h"
+#include "timer16n.h"
 
 #ifdef CONFIG_LINKLAY_UART
 void uart_driver_process(void);
@@ -14,6 +14,54 @@ void w2_4mac_driver_process(void);
 void plc_driver_process(void);
 void linklay_process(void);
 void app_process(void);
+
+
+void isr(void) interrupt 
+{	
+    uint8_t j;
+    
+	 if (T16G2IF && T16G2IE) {
+	     init_t16g1(0);
+        _sys_tick++;
+        _t16g1_valid = 1;
+	     T16G2IF = 0;
+     }
+
+    if (T16G1IF && T16G1IE) {       //1   
+        T16G1IE = 0; 
+	     T16G1IF = 0;        
+        T16G2IF = 0;
+        
+        switch (_plc_state) {        
+        case SEND: {    
+           DELAY3NOP();  //”ÎREC∂‘∆Î
+           if (_t16g1_valid) {          
+                init_t16g1(1);
+                _t16g1_valid = 0;
+            }
+            plc_send_proc();    
+            break;            
+            }            
+        case RECV: {
+            if (_t16g1_valid) {          
+                init_t16g1(1);
+                _t16g1_valid = 0;
+            }
+            plc_recv_proc();
+            DELAY1NOP();
+            break;
+            }
+        case IDLE: {
+            NOP();
+            break;
+            }
+        default:
+            NOP();
+            break;        
+        }
+    }
+
+}
 
 void sys_app_process()
 {
